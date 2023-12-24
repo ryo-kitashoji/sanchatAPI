fb9042f915d909a9d60ec8cc1fee8de8d46ecf10
ryo-kitashoji <ryo.kitashoji1998@gmail.com>
2023-12-24 16:45:24 +0900

    修正

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # サンタ風chatAPI
 rinnaのモデルにサンタ風のプロンプトを入力することでそれらしく振舞うようにしました．
+
+## 環境構築
+以下のコマンドを実行
+```
+git clone https://github.com/ryo-kitashoji/sanchatAPI.git
+cd sanchatAPI/
+python3 -m venv env # venv（pythonの仮想環境ツール）がない場合はpip install venvを実行
+source env/bin/activate
+pip install -r requirements.txt
+uvicorn main:app
+# 外部公開する場合は uvicorn main:app --host=0.0.0.0
+```
+
+## 実行方法
+```
+curl -X POST http://localhost:8000/ -H "Content-Type: application/json" -d "{\"message\":\"プレゼントをください\"}"
+```
--- main.py
+++ main.py
@@ -7,34 +7,33 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # すべてのオリジンを許可する場合
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # すべてのHTTPメソッドを許可する場合
-    allow_headers=["*"],  # すべてのHTTPヘッダーを許可する場合
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# リクエストデータのモデルを定義
 class SantaRequest(BaseModel):
     message: str
 
-# 日本語版GPT-2モデルのパイプラインを作成
-generator = pipeline('text-generation', model='rinna/japanese-gpt2-medium', max_length=50)
+def get_generator():
+    generator = pipeline('text-generation', model='rinna/japanese-gpt2-medium', max_length=50)
+    return generator
+
+def generate_text(message: str):
+    generator = get_generator()
+    response = generator(message, max_length=50)
+    return response[0]['generated_text']
 
 @app.post("/")
 def talk_to_santa(request: SantaRequest):
-    # ユーザーからのメッセージに基づいてレスポンスを生成
     santa_prompt = f"サンタさんは {request.message} についてどう思いますか？"
-    response = generator(santa_prompt, max_length=50)
+    generated_text = generate_text(santa_prompt)
     
-    # 生成されたテキストから質問文を取り除く
-    generated_text = response[0]['generated_text']
     try:
-        # 質問文の終わりを見つける
         end_of_question = generated_text.index("についてどう思いますか？") + len("についてどう思いますか？")
-        # 質問文以降のテキストを抽出
         answer = generated_text[end_of_question:].strip()
     except ValueError:
-        # 質問文が見つからない場合は、全テキストをそのまま使用
         answer = generated_text
-        
+    
     return {"response": answer}
